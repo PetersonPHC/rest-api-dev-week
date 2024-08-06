4f6852db6fef44f7fb02e00f857fdcefaa63f851
PetersonPHC <petersonhenryque@hotmail.com>
2024-08-06 14:44:01 -0300

    Initial commit

---
+++ src/test/java/phc/rest_api_dev_week/RestApiDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package phc.rest_api_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
