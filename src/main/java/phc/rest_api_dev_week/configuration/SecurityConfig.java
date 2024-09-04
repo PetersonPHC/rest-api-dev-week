7b2a2d90cdd8222b998884d52fe579ea6b47167d
PetersonPHC <petersonhenryque@hotmail.com>
2024-09-04 14:40:11 -0300

    Implements the final version for Basic Authentication in SecurityConfig.java

--- src/main/java/phc/rest_api_dev_week/configuration/SecurityConfig.java
+++ src/main/java/phc/rest_api_dev_week/configuration/SecurityConfig.java
@@ -2,15 +2,17 @@ package phc.rest_api_dev_week.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults; // Importação correta para withDefaults()
 
 @Configuration
 @EnableWebSecurity
@@ -19,23 +21,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()  // Exige autenticação para qualquer requisição
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/**").hasRole("USER")
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());  // Usa a nova forma recomendada para configurar a autenticação básica
-
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}123456")  // Usa senha em plain text para simplicidade
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("123"))
                 .roles("USER")
                 .build();
-
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
